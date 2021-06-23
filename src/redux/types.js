164e5c6e8b2cf9e9dd7c7f30a44e73b527556def
sashank12-hub <58355991+sashank12-hub@users.noreply.github.com>
2021-06-23 14:03:48 +0530

    Updates

--- src/redux/types.js
+++
@@ -1,8 +0,0 @@
-export const OPENMESSAGE = 'OPENMESSAGE'
-export const CLOSEMESSAGE = 'CLOSEMESSAGE'
-export const SENDMESSAGE = 'SENDMESSAGE'
-export const SELECTED = 'SELECTED'
-export const USER = 'USER'
-export const setTotal = 'setTotal'
-export const addtocart='addtocart'
-export const removefromcart='removefromcart'
